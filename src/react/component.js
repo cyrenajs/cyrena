f565b73d1a4bcb23b12cb0391ec1c045b81000ff
sarimarton <sarimarton@gmail.com>
2019-03-05 04:21:30 +0100

    Fix ReactRealm with deeper sub-vdoms

--- src/react/component.js
+++ src/react/component.js
@@ -1,10 +1,8 @@
-import xs, { Stream, MemoryStream } from 'xstream'
+import xs, { Stream } from 'xstream'
 import { h } from '@cycle/react'
-import {
-  makePragma,
-  component as powerCycleComponent
-} from '../component.js'
+import { makePragma, component as powerCycleComponent} from '../component.js'
 import { createElement, Fragment, useState, useEffect } from 'react'
+import cloneDeepWith from 'lodash/cloneDeepWith'
 
 export const pragma = makePragma(h)
 
@@ -12,37 +10,48 @@ const CONFIG = {
   vdomProp: 'react',
   combineFn: streams => xs.combine(...streams),
   mergeFn: streams => xs.merge(...streams),
-  isStreamFn: val => val && val instanceof Stream || val instanceof MemoryStream
+  isStreamFn: val => val instanceof Stream
 }
 
+const isReactComponent = val =>
+  val && val.$$typeof === Symbol.for('react.element') &&
+    typeof val.type === 'function'
+
+// This cycle component does the followings:
+// - prepares a reducer$ stream early to have it in the sinks object
+// - traverses the sub-vdom deeply, regardless of component boundaries
+//   (they're all react components, we're not invoking them), and amends the
+//   components' props objects with the necessary cycle information, later
+//   used in the hooks
 export function ReactRealm (sources) {
   const reducer$ = xs.create({
     start: function () {},
     stop: function () {}
   })
 
+  const subVdomWithAmendedProps = cloneDeepWith(
+    sources.props.children,
+    val => isReactComponent(val)
+      ? {
+        ...val,
+        props: {
+          ...val.props,
+          sources: { ...sources, reducer$ }
+        }
+      }
+      : undefined
+  )
+
   return {
     react: xs.of(
-      createElement(
-        Fragment,
-        null,
-        [sources.props.children]
-          .flat()
-          .map((cmp, idx) =>
-            cmp && cmp.$$typeof === Symbol.for('react.element')
-              ? {
-                ...cmp,
-                key: cmp.key != null ? cmp.key : idx,
-                props: { ...cmp.props, sources: { ...sources, reducer$ } }
-              }
-              : cmp
-          )
-      )
+      createElement(Fragment, null, subVdomWithAmendedProps)
     ),
     state: reducer$
   }
 }
 
+// React hook to handle cycle state. The sources object is passed
+// to every react component in props under a ReactRealm component.
 export function useCycleState (sources) {
   const [state, setState] = useState(0)
 
@@ -60,6 +69,8 @@ export function useCycleState (sources) {
   ]
 }
 
+// This is just a dummy component to serve as a lens boundary for
+// a sub-vdom. Use lens prop on it, just as on any other cycle component
 export function Scope (sources) {
   return component(
     createElement(Fragment, null, sources.props.children),
@@ -68,6 +79,9 @@ export function Scope (sources) {
   )
 }
 
+// Wrapper for any cycle component for the convenience of shorthand
+// return values. An initial component() call remembers the sources object,
+// and passes it through every invocation through the component() call tree.
 export function withPower (Cmp) {
   return function (sources) {
     return component(createElement(Cmp), null, sources)
