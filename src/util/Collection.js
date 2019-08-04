bf36034a7adb574f165390d8f7a2e98a2daf4558
sarimarton <sarimarton@gmail.com>
2019-08-04 04:07:20 +0200

    Add 'outerState' key to the Collection item wrapper lens, and make it possible to modify them with the reducer value

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -11,19 +11,6 @@ import {
 export const COLLECTION_DELETE =
   prevState => undefined
 
-const itemWrapperLens = {
-  get: state => state.map((item, index) => {
-    return {
-      item,
-      index,
-      collection: state
-    }
-  }),
-  set: (state, childState) => {
-    return childState.map(record => record.item)
-  }
-}
-
 export const CollectionItem = sources =>
   powercycle(
     pragma(Fragment, null, ...castArray(sources.props.children)),
@@ -55,51 +42,92 @@ export const collectSinksBasedOnSource = sources => instances => {
 }
 
 export function Collection (sources) {
-  const noWrap = sources.props.nowrap
   const outerStateName = sources.props.outerstate === undefined
     ? 'outerState'
     : sources.props.outerstate
   const channel = sources.props.channel || 'state'
 
-  const listSinks = [0]
-    .map(() =>
-      makeCollection({
-        item: CollectionItem,
-        // I'm not sure what it's for. From cycle's source, it seems like that it
-        // serves as an isolation base, but we already have isolation on the items...
-        // itemKey: (childState, index) => String(index),
-        channel,
-        itemScope: sources.props.itemscope || (key => key),
-        collectSinks: collectSinksBasedOnSource({
-          ...sources,
-          // Value doesn't matter here, just add the outerstate key for pickMerge
-          ...outerStateName && { [outerStateName]: 1 }
-        })
+  const forLens = !sources.props.for
+    ? { get: state => state, set: (state, childState) => childState }
+    : resolveDotSeparatedScope(sources.props.for).state
+
+  const collectionCmp = makeCollection({
+    item: CollectionItem,
+    // I'm not sure what it's for. From cycle's source, it seems like that it
+    // serves as an isolation base, but we already have isolation on the items...
+    // itemKey: (childState, index) => String(index),
+    channel,
+    itemScope: sources.props.itemscope || (key => key),
+    collectSinks: collectSinksBasedOnSource({
+      ...sources,
+      // Value doesn't matter here, just add the outerstate key for pickMerge
+      ...outerStateName && { [outerStateName]: 1 }
+    })
+  })
+
+  // This part wraps the item states in an object which provides access to all
+  // the needed information for the item:
+  // - item - the item state itself
+  // - index - the item index in the collection
+  // - collection - the collection array
+  // - outerState - the whole outer state
+  //
+  // The regular state sink of the item receives reducers of this wrapper object.
+  // The lens will read the 'item' key, but the collection and outerState keys
+  // can be returned modified as well. The lens will detect if there's a change
+  // on these outer objects, and takes that into account. This is a bit smelly
+  // from and FP standpoint, but it keeps user code terse and clear without
+  // having to rely on convoluted outerState: ev$ -> ev$.compose(state) stuff.
+  // However, that solution keeps being available too.
+  const listCmpWithSpecialItemScope = [collectionCmp]
+    .map(list => {
+      return isolate(list, {
+        state: {
+          get: state => {
+            return forLens.get(state)
+              .map((item, index, collection) => ({
+                item, index, collection, outerState: state
+              }))
+          },
+          set: (state, childState) => {
+            let outerStateChanged = false
+            let collectionChanged = false
+
+            const collection = forLens.get(state)
+
+            const setterForItemWrap = (state, childState) =>
+              childState.map(record => {
+                if (state !== record.outerState) {
+                  outerStateChanged = { value: record.outerState }
+                }
+                if (collection !== record.collection) {
+                  collectionChanged = { value: record.collection }
+                }
+
+                return record.item
+              })
+
+            const result =
+              forLens.set(state, setterForItemWrap(state, childState))
+
+            if (collectionChanged) {
+              return forLens.set(state, collectionChanged.value)
+            }
+            if (outerStateChanged) {
+              return outerStateChanged.value
+            }
+
+            return result
+          }
+        }
       })
-    )
-
-    // Wrap items in a record, holding 3 keys: item, index, collection
-    .map(list =>
-      noWrap
-        ? list
-        : isolate(list, {
-            [channel]: itemWrapperLens
-          })
-    )
-
-    // Resolve 'for' prop. The 'for' prop has the same effect of 'scope', except
-    // that it doesn't scope down the outerState stream in the items so they
-    // can have full access to the outer state
-    .map(list =>
-      sources.props.for
-        ? isolate(list, resolveDotSeparatedScope(sources.props.for))
-        : list
-    )
+    })
 
     // Add outerState to sources
     .map(list =>
       outerStateName
         ? sources => {
+            // outerStateWrapperLens =
             const sinks = list({
               ...sources,
               // It only works with streams, donno why
@@ -113,9 +141,11 @@ export function Collection (sources) {
           }
         : list
     )
-
-    .map(list => pragma(list, null, ...castArray(sources.props.children)))
   [0]
 
-  return listSinks
+  return pragma(
+    listCmpWithSpecialItemScope,
+    null,
+    ...castArray(sources.props.children)
+  )
 }
