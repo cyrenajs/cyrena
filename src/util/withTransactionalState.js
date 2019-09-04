f32263123b2dd4a96638f105e4c9366deb6024d0
sarimarton <sarimarton@gmail.com>
2019-09-04 02:54:17 +0200

    Fix withTransactionalState's missing initial state sampling and cleanup

--- src/util/withTransactionalState.js
+++ src/util/withTransactionalState.js
@@ -3,25 +3,25 @@ import withPower from 'powercycle'
 import xs from 'xstream'
 import sampleCombine from 'xstream/extra/sampleCombine'
 
-export function withTransactionalState (reducerOrActionsObject, cmp) {
-  const reducer = typeof reducer === 'function'
-    ? makeReducer(reducerOrActionsObject)
-    : reducerOrActionsObject
+export const makeAction = name => payload => [name, payload]
 
+export function withTransactionalState (reducer, cmp) {
   return withState(sources => {
     const sinks = withPower(cmp)(sources)
 
     sinks.state =
       (sinks.state || xs.never())
-        .compose(sampleCombine(sources.state.stream))
+        .compose(
+          sampleCombine(sources.state.stream.startWith(undefined))
+        )
         .map(([actionOrReducer, prevState]) => {
-
           if (typeof actionOrReducer === 'function') {
-            return actionOrReducer
+            console.warn('withTransactionalState: function reducer received ' +
+              'instead of action.', actionOrReducer)
 
+            return actionOrReducer
           } else {
             return () => reducer(prevState, actionOrReducer)
-
           }
         })
         .startWith(() => reducer(undefined, []))
